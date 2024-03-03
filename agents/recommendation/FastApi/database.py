9d00486eecdafdb0965751f009f4881a4f72f3c5
RayDu <97069739+CongruiDu@users.noreply.github.com>
2024-03-02 17:24:11 -0800

    modify database.py for firebase

--- agents/recommendation/FastApi/database.py
+++ agents/recommendation/FastApi/database.py
@@ -1,31 +1,41 @@
-import motor.motor_asyncio
-from model import User
-from collections.abc import MutableMapping
+## this file is used to make changes to the user_preference collection 
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
-database=client.User
-collection=database.user_preference
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from model import User
+
+
+
+
+cred = credentials.Certificate('../gencalendar-9a8b4-firebase-adminsdk-tmzfi-82ec806343.json')
+firebase_admin.initialize_app(cred)
+
+# Get a reference to Firestore
+db = firestore.client()
+collection = db.collection('users')
+
 
 async def fetch_one_user(user_id: int):
-    document = await collection.find_one({"user_id": user_id})
+    document = await collection.where("user_id", "==", user_id).get()
     return document
 
 async def fetch_all_users():
     users = []
-    async for document in collection.find({}):
-        users.append(User(**document))
+    async for user in collection.stream():
+        users.append(user.to_dict())
     return users
 
 async def create_user(user: User):
     document = user
-    await collection.insert_one(document)
+    await collection.add(document.dict())
     return document
 
 async def update_user(user_id: int, user: User):
-    await collection.update_one({"user_id": user_id}, {"$set": user.dict()})
-    document = await collection.find_one({"user_id": user_id})
+    await collection.where('user_id', '==', user_id).update(user.dict())
+    document = await collection.where('user_id', '==', user_id).get()
     return document
 
 async def remove_user(user_id: int):
-    await collection.delete_one({"user_id": user_id})
+    await collection.where('user_id', '==', user_id).delete()
     return True
